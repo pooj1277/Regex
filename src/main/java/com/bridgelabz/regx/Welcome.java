7612e625f5e133c90e0fa03abc529a6b7cb420a7
pooj1277 <pooja.sd3@gmail.com>
2021-10-17 12:11:00 -0700

    Welcome to User Registration problem

---
+++ src/main/java/com/bridgelabz/regx/Welcome.java
@@ -0,0 +1,12 @@
+/*
+ * Problem : Display Welcome message to the master branch
+ * Author: Pooja Avhad
+ */
+
+package com.bridgelabz.regx;
+
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("Welcome to day 19 Regex - User Registration problem.");
+	}
+}
