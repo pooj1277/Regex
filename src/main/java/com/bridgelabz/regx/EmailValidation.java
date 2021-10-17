2f23c1c24c9f928408f53c8aed84470507041b95
pooj1277 <pooja.sd3@gmail.com>
2021-10-17 12:36:19 -0700

    [Added]Email Validation for abc.xyz@bl.co.in format

--- src/main/java/com/bridgelabz/regx/EmailValidation.java
+++ src/main/java/com/bridgelabz/regx/EmailValidation.java
@@ -1,6 +1,6 @@
 /*
  * Problem : As a User need to enter a valid email - E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions 
- * Author: Pooja Avhad
+ * Author: Pooja Avhad.
  */
 package com.bridgelabz.regx;
 import java.util.regex.Pattern;
